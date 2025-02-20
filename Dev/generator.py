062a6cca77c7a8b56ae1ba04fcff7047d4265361
sumiran007 <sumiranracherla@gmail.com>
2025-02-20 18:32:33 +0000

    gen

--- Dev/generator.py
+++ Dev/generator.py
@@ -1,17 +1,27 @@
 import pygame 
+rangen = open('random_generations.txt', 'a')
 import random
 water = []
 for i in range(10):
     water.append(['w'])
 print(water)
-randoms = []
+
 translate = {
         'w': 'water',
         'r': 'rock',
         'g': 'grass',
         'l': 'log',
     }
-for j in range(10):
-    randoms.append(random.choice(['w', 'r', 'g', 'l']))
+
+
+
+for z in range (200):
+    randoms = []
+    for j in range(10):
+        randoms.append(random.choice(['w', 'r', 'g', 'l']))
+    rangen.write(str(randoms))
+    rangen.write('\n')
 print(randoms)
+rangen.close()
+
     
