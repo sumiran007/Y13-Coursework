cadbe7ae427cfc7f2a6a676024ac3831a0eef995
sumiran007 <sumiranracherla@gmail.com>
2025-04-06 20:27:21 +0100

    add a login app

--- Dev/pyqt.py
+++ Dev/pyqt.py
@@ -1,33 +1,72 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QVBoxLayout
-#all the imports from the library
-app = QApplication(sys.argv)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLineEdit, QVBoxLayout, 
+                             QPushButton, QLabel, QFrame)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
-window = QWidget()
-#set the window like pygame.init()
-window.setWindowTitle('Password Box Example')
-#sets the title of the window
-window.resize(300, 100)
-#sets the size of the window
-layout = QVBoxLayout()
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle('Login')
+        self.setFixedSize(350, 350)
 
-password_box = QLineEdit()
-username_box = QLineEdit()
-username_box.setPlaceholderText('Enter your username')
-password_box.setPlaceholderText('Enter your password')
+        # Main container
+        container = QFrame(self)
+        container.setGeometry(25, 25, 300, 300)
 
-#creates a text box to type into
-#makes the text look hidden so that the user can't see what they are typing
-username_box.setEchoMode(QLineEdit.Normal)
-password_box.setEchoMode(QLineEdit.Password)  
+        layout = QVBoxLayout(container)
+        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setSpacing(10)
 
-#sets the text box to be normal text
-layout.addWidget(username_box)
-layout.addWidget(password_box)
+        # Login header
+        login_label = QLabel("Login")
+        login_label.setAlignment(Qt.AlignCenter)
+        login_label.setFont(QFont("Arial", 20))
+        login_label.setObjectName("loginLabel")  # For targeted styling
 
-window.setLayout(layout)
-window.show()
-#outputs the window like pygame.display.update()
-sys.exit(app.exec_())
-#exits the program like pygame.quit()
-#no point of using oop since most of the code is in the library
+        # Username label
+        username_label = QLabel("Username")
+
+        # Username input
+        self.username_input = QLineEdit()
+
+        # Password label
+        password_label = QLabel("Password")
+
+        # Password input
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+
+        # Login button
+        login_button = QPushButton("Login")
+        login_button.setObjectName("loginButton")
+
+        # Sign up button
+        signup_button = QPushButton("Sign Up")
+        signup_button.setObjectName("signupButton")
+
+        layout.addWidget(login_label)
+        layout.addStretch(1)
+        layout.addWidget(username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(password_label)
+        layout.addWidget(self.password_input)
+        layout.addStretch(1)
+        layout.addWidget(login_button)
+        layout.addWidget(signup_button)
+
+        self.show()
+
+def load_stylesheet(path):
+    with open(path, "r") as file:
+        return file.read()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    style = load_stylesheet("style.css")
+    app.setStyleSheet(style)
+    window = LoginWindow()
+    sys.exit(app.exec_())
